e2f91539eae90d2aed8e96ddedef2350421a5750
Pouriya <pouriya.jamshidi@inmanta.com>
2022-05-19 13:50:06 +0200

    add docstring for ipnet plugin

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -70,6 +70,14 @@ def net_to_nm(network_addr: "string") -> "string":
 
 @plugin
 def ipnet(addr: "ip::cidr_v10", what: "string") -> "string":
+    """
+    Return the ip, prefixlen, netmask or network address of the CIDR
+
+    :param ip: The IP address of `addr` object
+    :param prefixlen: The prefix length of `addr` object
+    :param netmask: The subnet mask of `addr` object
+    :param network: The network address of `addr` object
+    """
     net = netaddr.IPNetwork(addr)
     if what == "ip":
         return str(net.ip)
